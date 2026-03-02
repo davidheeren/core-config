cfa25d212bed518c43b00dceca153e4fbf5a8f8f
= <=>
2026-03-01 19:53:12 -0600

    working install script

--- install/win-install.ps1
+++ install/win-install.ps1
@@ -1,4 +1,20 @@
 
+# Make necessary directories
+# Symlink file if it doesn't exist already
+function New-SymLink($Path, $Target)
+{
+    if (Test-Path $Path)
+    { 
+        Write-Host "Skipping: $Path"
+        return 
+    }
+
+    New-Item -ItemType Directory (Split-Path $Path) -Force | Out-Null
+    New-Item -ItemType SymbolicLink -Path $Path -Target (Resolve-Path $Target).Path | Out-Null
+    Write-Host "Linked: $Path"
+}
+
+# Install apps
 winget install Neovim.Neovim
 winget install marlocarlo.psmux
 winget install sharkdp.fd
@@ -6,10 +22,13 @@ winget install ajeetdsouza.zoxide
 winget install junegunn.fzf
 winget install Microsoft.PowerShell
 
+# Install powershell modules
 Install-Module -Name PSFzf
 Install-Module -Name PSReadLine
 
-# TODO: Check for existing folder and file
-New-Item -ItemType SymbolicLink -Path "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Target (Resolve-Path "..\powershell\Microsoft.PowerShell_profile.ps1").Path
-New-Item -ItemType SymbolicLink -Path "$HOME\AppData\Local\nvim\init.lua" -Target (Resolve-Path "..\nvim\init.lua").Path
-New-Item -ItemType SymbolicLink -Path "$HOME\.config\psmux\psmux.conf" -Target (Resolve-Path "..\tmux\tmux.conf").Path
+# Symlink config files
+New-SymLink "$HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" "..\powershell\Microsoft.PowerShell_profile.ps1"
+New-SymLink "$HOME\AppData\Local\nvim\init.lua" "..\nvim\init.lua"
+New-SymLink "$HOME\.config\psmux\psmux.conf" "..\tmux\tmux.conf"
+
+Write-Host "All done. Please update the terminal to use plain Powershell by default"
